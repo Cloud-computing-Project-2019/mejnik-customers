dab068aa88ef5f433cd0f16123e86c33263778e8
ghajduk3 <hajdukovicgojko3@gmail.com>
2019-12-02 13:50:40 +0100

    Add changes

---
+++ src/main/java/si/fri/rso/mejnik/customers/MejnikApplication.java
@@ -0,0 +1,12 @@
+package si.fri.rso.mejnik.customers;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Benjamin Kastelic
+ * @since 2.3.0
+ */
+@ApplicationPath("v1")
+public class MejnikApplication extends Application {
+}
